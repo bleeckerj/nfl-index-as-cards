9f25018a588c5fa32021fa211ce9175b244e4851
Julian <bleeckerj@gmail.com>
2025-12-25 08:06:41 -0800

    Tag

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  safelist: ['text-xl', 'font-mono'],
   theme: {
     extend: {},
   },
